5a34f64cd10539b1472c11dd6321c46cda06bd04
심향택 <spprid@hanmail.net>
2017-11-08 23:57:35 +0900

    `

---
+++ SproutTable/Util.swift
@@ -0,0 +1,16 @@
+//
+//  Util.swift
+//  SproutTable
+//
+//  Created by Spprid on 2017. 11. 8..
+//  Copyright © 2017년 Spprid. All rights reserved.
+//
+
+import Foundation
+
+class Util
+{
+    init(){
+        
+    }
+}
