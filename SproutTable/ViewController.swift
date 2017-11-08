f079a581dbbbddd8f28186e07b9eef2ce5273477
심향택 <spprid@hanmail.net>
2017-11-09 01:24:56 +0900

    HTTP 통신 후 JSON 받아오기

--- SproutTable/ViewController.swift
+++ SproutTable/ViewController.swift
@@ -13,15 +13,28 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblLunch: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let jsonUrl = "https://gw.sinsago.co.kr/m/rice_json.aspx"
         
         Alamofire.request(jsonUrl).responseJSON(){ response in
-            
-            print(response.result.isSuccess)
-            
+            if response.result.isSuccess{
+                let field = ["lunch", "dinner"]
+                
+                let json = JSON(response.result.value)
+                let lunch = json["content"]["lunch"]
+                
+                var nm : String = ""
+                
+                for (_, JSON) in lunch
+                {
+                    nm += JSON.stringValue + "\r"
+                }
+                
+                self.lblLunch.text = nm
+            }
         }
     }
 
